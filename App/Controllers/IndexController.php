013c00b70c647a20bd555297e171b1e15d662eb2
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-07-12 21:35:33 -0300

    feature: cadastro de usuarios

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -14,7 +14,50 @@ class IndexController extends Action {
 	}
 
 	public function inscreverse() {
+		$this->view->usuario = array(
+			'nome'=>'', 
+			'email'=>'', 
+			'senha'=>'');
+
+		$this->view->errorCadastro = false;
+
+		$this->view->jaCadastrado = false;
+
 		$this->render('inscreverse');
+	}
+
+	public function registrar(){
+
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		if($usuario->validarCadastro()){
+
+			if(count($usuario->getUsuarioPorEmail())== 0){
+				$usuario->salvar();
+
+				$this->render('cadastro');
+			}else{
+				$this->view->usuario = array('nome'=>$_POST['nome'], 'email'=>$_POST['email'], 'senha'=>$_POST['senha']);
+
+				$this->view->errorCadastro = false;
+				$this->view->jaCadastrado = true;
+				$this->render('inscreverse');
+
+			}
+			
+		}else{
+			$this->view->usuario = array('nome'=>$_POST['nome'], 'email'=>$_POST['email'], 'senha'=>$_POST['senha']);
+
+			$this->view->errorCadastro = true;
+
+			$this->view->jaCadastrado = false;
+
+			$this->render('inscreverse');
+		}
 	}
 
 }
--- App/Route.php
+++ App/Route.php
@@ -20,6 +20,13 @@ class Route extends Bootstrap {
 			'action' => 'inscreverse'
 		);
 
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
+
+
 		$this->setRoutes($routes);
 	}
 
